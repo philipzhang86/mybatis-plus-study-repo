0719d55088d8049d43a5cab463d3e2d4d1660321
philipzhang <xiaoxiaoxiao67@gmail.com>
2024-02-22 20:29:20 -0500

    Project has bugs for some reason 1.0

--- mybatis-plush-base-quick-01/src/main/java/com/jmalltech/pojo/User.java
+++ mybatis-plush-base-quick-01/src/main/java/com/jmalltech/pojo/User.java
@@ -1,5 +1,6 @@
 package com.jmalltech.pojo;
 
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import lombok.Data;
 @TableName("user")
 @Data
 public class User {
+    @TableId
     private Long id;
     private String username;
     private Integer age;
